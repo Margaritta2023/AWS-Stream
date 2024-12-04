eaf586af6e98dd1fd37fd8611c60e524bf398864
Margaritta2023 <margarittamkn@gmail.com>
2024-12-04 17:15:54 +0400

    initial commit

---
+++ task06/app/lambdas/audit_producer/index.js
@@ -0,0 +1,60 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+    const tableName = 'cmtr-63edc6d2-Audit-test';
+
+    for (const item of event.Records) {
+        if (!item) continue;
+
+        const eventName = item.eventName;
+        const newImage = AWS.DynamoDB.Converter.unmarshall(item.dynamodb.NewImage);
+        const oldImage = item.dynamodb.OldImage ? AWS.DynamoDB.Converter.unmarshall(item.dynamodb.OldImage) : null;
+
+        if (eventName === 'INSERT') {
+            const itemKey = newImage.key || '';
+            const itemValue = newImage.value || '';
+            console.log('Item Key:', itemKey);
+            console.log('Item Value:', itemValue);
+
+            const item = {
+                id: uuidv4(),
+                itemKey: itemKey,
+                modificationTime: new Date().toISOString(),
+                newValue: {
+                    key: itemKey,
+                    value: itemValue
+                }
+            };
+
+            await dynamodb.put({
+                TableName: tableName,
+                Item: item
+            }).promise();
+        } else if (eventName === 'MODIFY') {
+            const itemKey = newImage.key || '';
+            const newValue = newImage.value || '';
+            const oldValue = oldImage ? oldImage.value || '' : '';
+
+            const item = {
+                id: uuidv4(),
+                itemKey: itemKey,
+                modificationTime: new Date().toISOString(),
+                updatedAttribute: 'value',
+                oldValue: oldValue,
+                newValue: newValue
+            };
+
+            await dynamodb.put({
+                TableName: tableName,
+                Item: item
+            }).promise();
+        }
+
+        console.log('Processed record:', JSON.stringify(item));
+    }
+
+    return null;
+};
